ac869655c0ad3e0cc4c6f66cac5a3bbb68bc3fb8
MichaelRyan23 <142366498+MichaelRyan23@users.noreply.github.com>
2023-11-12 18:54:14 -0600

    Changed name, added Instruction register logic maybe?

--- Instruction Register.sv
+++ Instruction Register.sv
@@ -1,7 +1,14 @@
-module reg10(
-	input logic [9:0] D,
-	input logic EN, CLKb,
-	output logic [9:0] Q
+module insRegister(
+    input logic [9:0] D,    // 10-bit data input
+    input logic EN, CLKb,   // Enable and clock signals
+    output logic [9:0] Q    // 10-bit data output
 );
-	
+
+    // On the falling edge of the clock, if enable is high, update the output Q
+    always_ff @(negedge CLKb) begin
+        if (EN) begin
+            Q <= D;
+        end
+    end
+
 endmodule
