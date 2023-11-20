a11b0af45c4b496b3194def27a247629e6ee6116
Kale Nelson <143835703+Sor3n007@users.noreply.github.com>
2023-11-19 19:51:29 -0600

    A thing that may or may not work. (Reversable)

--- Multi_state_ALU.sv
+++ Multi_state_ALU.sv
@@ -7,24 +7,33 @@ module ALU(
 	logic [9:0]Temp;
 	logic [9:0]_Q;
 	logic [9:0]_G;
+	
+	always_comb
+	begin
+		case(FN)
+			4'b0001: _Q = OP + Temp;
+			4'b0010:	_Q = OP - Temp;
+			4'b0100: _Q = OP & Temp;
+			4'b1000: _Q = OP | Temp;
+			default: _Q = 10'b0;//default to handle exeptions.
+		endcase
+	end
+	
 	always_ff @(negedge CLKb)
 	begin
 		if(Ain) 
 		begin
 			Temp <= OP;
-			case(FN)
-				4'b0001: _Q = OP + Temp;
-				4'b0010:	_Q = OP - Temp;
-				4'b0100: _Q = OP & Temp;
-				4'b1000: _Q = OP | Temp;
-				default: _Q = 10'b0;//default to handle exeptions.
-			endcase
+			
 		end
 		if(Gin) 
 		begin
 			_G <= _Q;
 		end
-		if(Gout) trireg10(.D(_G), .CLKb(CLKb), .Rin(Gout), .Rout(Gout), .Q(Q));
+		if(Gout) 
+		begin
+			trireg10(.D(_G), .CLKb(CLKb), .Rin(Gout), .Rout(Gout), .Q(Q));
+		end
 	end
 	
 endmodule
