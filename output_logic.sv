656812e45291cecea193773f3130e2dc6fb75595
Kale Nelson <143835703+Sor3n007@users.noreply.github.com>
2023-11-29 12:00:50 -0600

    fixed

--- output_logic.sv
+++ output_logic.sv
@@ -16,32 +16,27 @@ module output_logic(
 	logic [3:0]REG0, REG1, REG2;
 	
 	assign LED_B = BUS;
-	assign BUS0 = BUS[3:0];
-	assign BUS1 = BUS[8:4];
-	assign BUS2 = BUS[9];
-	
-	assign REG0 = REG[3:0];
-	assign REG1 = REG[8:4];
-	assign REG2 = REG[9];
-	
-	seven_seg hex6(.A(TIME), .S(THEX));
 	always_comb
 	begin
 		if(PEEKb)
 		begin
-			seven_seg hex0(.A(BUS0), .S(DHEX0));//seven segs will need to be changed if we use yours
-			seven_seg hex1(.A(BUS1), .S(DHEX1));//which considering you have a lot of the code we should do that
-			seven_seg hex2(.A(BUS2), .S(DHEX2));//unsure if this will work. sorry
+			assign BUS0 = BUS[3:0];
+			assign BUS1 = BUS[8:4];
+			assign BUS2 = BUS[9];
 		end
-		else 
+		else
 		begin
-			seven_seg hex3(.A(REG0), .S(DHEX0));//nearly the same as the previous
-			seven_seg hex4(.A(REG1), .S(DHEX1));
-			seven_seg hex5(.A(REG2), .S(DHEX2));
+			assign BUS0 = REG[3:0];
+			assign BUS1 = REG[8:4];
+			assign BUS2 = REG[9];
 		end
 		if(DONE) LED_D = 1;
 		else LED_D = 0;
-	end
+	end	
+	seven_seg hex0(.A(BUS0), .S(DHEX0));//seven segs will need to be changed if we use yours
+	seven_seg hex1(.A(BUS1), .S(DHEX1));//which considering you have a lot of the code we should do that
+	seven_seg hex2(.A(BUS2), .S(DHEX2));//unsure if this will work. sorry
+	seven_seg hex6(.A(TIME), .S(THEX));
 	
 
 endmodule
